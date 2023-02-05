79a5cc39674f03ae753bede15861ea59398462dd
pouriaafshari <pooriyaafshari81@gmail.com>
2023-02-05 18:45:15 +0000

    test get request to Pythonanywhere

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [response, setResponse] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch('http://pooriya007.pythonanywhere.com/');
+      const text = await res.text();
+      setResponse(text);
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hai :3
+          {
+            response
+          }
         </p>
       </header>
     </div>
